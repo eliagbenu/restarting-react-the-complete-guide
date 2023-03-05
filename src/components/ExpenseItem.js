6acb2661cd2d31f01357b0d53c63cdacdaa7d72b
agb3nu <agb3nu@gmail.com>
2023-03-05 11:11:49 +0000

    added lesson 37

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -4,19 +4,32 @@ import './ExpenseItem.css';
 function ExpenseItem(props)
 {
 
+	var theAmount =props.amount; 
+	var theTitle =props.title;
+	var theDate =props.thedate.toISOString();
+	var theMonth =props.thedate.toLocaleString('en-US',{month:'long'});
+	var theDay =props.thedate.toLocaleString('en-US',{day:'2-digit'});
+	var theYear =props.thedate.getFullYear();
+
 	return ( <div className="default-item">
 		<div>
-		<h1>Title:{props.title}</h1>
+		<h1>Title:{theTitle}</h1>
 		</div>
 		<div>
-		<p>Date: {props.thedate.toISOString()}</p>
+		<p>
+			<div>Date: {theDate} </div>
+			<div>Month: {theMonth} </div>
+			<div>Day: {theDay} </div>
+			<div>Year: {theYear} </div>
+		</p>
 		</div>		
 		<div>
-		<h4>Amount: ${props.amount} Ghana Cedis</h4>
+		<h4>Amount: ${theAmount} Ghana Cedis</h4>
 		</div>		
 		<hr/>
 		</div>
 	 );
+
 }
 
 export default ExpenseItem;
